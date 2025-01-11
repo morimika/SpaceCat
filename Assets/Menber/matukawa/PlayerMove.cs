24aa9a7a5a93fe32c28e70c7ab37f3884f43a0bf
MatsukawaHannahEmily <107166878+MatsukawaHannahEmily@users.noreply.github.com>
2025-01-11 13:48:48 +0900

    きゅうりと魚が降ってくる

--- Assets/Menber/matukawa/PlayerMove.cs
+++ Assets/Menber/matukawa/PlayerMove.cs
@@ -7,23 +7,21 @@ using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
+using Unity.VisualScripting;
+using UnityEditor.VersionControl;
+using NaughtyAttributes;
 
-
+// Matsukawa
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] GameObject player;
-    [SerializeField] private float _cycle = 1;
-    private float speed = 8.0f;
-    public GameObject target;
+    // Playerの移動関係
+    private float _speed = 8.0f;
 
+    // エフェクト
+    [SerializeField, Label("魚に当たった時のエフェクト")] GameObject _fishEffects;
     public Renderer _fadeTarget;
 
-    private Vector3 mousePosition;
-    private Vector3 objPosition;
-
-    [SerializeField] GameObject enemyNiBikkuri;
-    [SerializeField] GameObject fishNiHappy;
-
+    // 他スクリプトの参照
     [SerializeField]
     private ResultManager_Mori _re;
 
@@ -35,52 +33,104 @@ public class PlayerMove : MonoBehaviour
     private bool _isGoal = false;
     private void Start()
     {
-       // CreateItems();
-       _rigi= GetComponent<Rigidbody2D>();  
+        _rigi = GetComponent<Rigidbody2D>();  
 
     }
     void Update()
     {
         // スタート位置、ターゲットの座標、速度
-        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float moveY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * _speed;
+        float moveY = Input.GetAxis("Vertical") * Time.deltaTime * _speed;
 
         // 画面外に出さない
         transform.position = new Vector2(
         //エリア指定して移動する
-        Mathf.Clamp(transform.position.x + moveX, -8.5f, 8.5f),
-        Mathf.Clamp(transform.position.y + moveY, -10000f, 10000f)
+        Mathf.Clamp(transform.position.x + moveX, -2f, 2f),
+        Mathf.Clamp(transform.position.y + moveY, -5f, 5f)
         );
 
+
+
+        // リザルトへ行く処理
         if (this.gameObject.transform.position.y >= 0&&!_isGoal)
         {
-            _re.Resulu();
+            // _re.Resulu();
             PlayerLayer._doFollow = false;
             _isGoal = true;
             _rigi.velocity=Vector2.zero;
             _rigi.gravityScale = -0.02f;
+
+            // 画面外に出さない
+            transform.position = new Vector2(
+            //エリア指定して移動する
+            Mathf.Clamp(transform.position.x + moveX, -2f, 2f),
+            Mathf.Clamp(transform.position.y + moveY, -6f, 1000f)
+            );
+
+            if (this.gameObject.transform.position.y == 1500)
+            {
+                // 位置が1500をになったら止まる
+                _rigi.gravityScale = 0f;
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "shark")
+        //プレイヤーと目標のアイテムの距離を取得
+        float _distance = Vector3.Distance(this.transform.position, other.transform.position);
+        
+        if (other.gameObject.tag == "Cucuember")
         {
             //GameOverを表示する
-            Debug.Log("shark");
-            ResultManager_Mori.CrashValue = ResultManager_Mori.CrashValue + 1;
-            Instantiate(_eff, transform.position, Quaternion.identity);
+            Debug.Log("きゅうり");
+
+            //ResultManager_Mori.UriValue = ResultManager_Mori.UriValue + 1;
+            //Instantiate(_eff, transform.position, Quaternion.identity);
+
         }
-        if (other.gameObject.tag == "cucuember")
+        if (other.gameObject.tag == "Fish")
         {
-            //GameOverを表示する
-            Debug.Log("cucuember");
+            Vector3 _force = other.transform.position - this.gameObject.transform.position; // 力の方向を指定
+            _rigi.AddForce(_force * 0.01f, ForceMode2D.Impulse); // 力を加えて移動
 
-            Debug.Log("cucuember");
-            ResultManager_Mori.UriValue = ResultManager_Mori.UriValue + 1;
-            Instantiate(_eff, transform.position, Quaternion.identity);
+            Debug.Log(("魚発見") + _distance);
+            //ResultManager_Mori.UriValue = ResultManager_Mori.UriValue + 1;
+            //Instantiate(_eff, transform.position, Quaternion.identity);
 
         }
+
     }
+
+    //きゅうりが画面内にいたら離れる
+    //魚なら近づく
+    //public void ItemDistance(GameObject targetObj)
+    //{
+    //    プレイヤーと目標のアイテムの距離を取得
+    //    float _distance = Vector3.Distance(this.transform.position, targetObj.transform.position);
+
+    //    if (_distance <= 18f && targetObj.tag == "Cucumber")
+    //    {
+    //        Addforce離れる
+    //        Debug.Log(("きゅうり") + _distance);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("きゅうりなし");
+    //    }
+
+    //    if (_distance <= 18f && GameObject.Find("Fish"))
+    //    {
+    //        Addforce近づく
+    //       Vector3 _force = targetObj.transform.position; // 力の方向を指定
+    //        _rigi.AddForce(_force, ForceMode2D.Force); // 力を加えて移動
+
+    //        Debug.Log(("魚発見") + _distance);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("さかななし");
+    //    }
+    //}
 }
 
